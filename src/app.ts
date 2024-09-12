16f14def0adacb7c8835ea3dab152f2520c9beae
leoli <jafee201153@gmail.com>
2024-09-12 23:18:06 +0800

    refactor(app.ts): reverse versions list and add version labels for better readability in output JSON

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,26 @@ import * as gcc from './gcc';
 
 async function main() {
     try {
-        const versionsList = gcc.availableVersions();
-        const versionsData: { [gccRelease: string]: { [platform: string]: { url: string; md5: string | null } } } = {};
+        let versionsList = gcc.availableVersions();
 
+        versionsList = versionsList.reverse();
+
+        const versionsData: { [versionLabel: string]: { [gccRelease: string]: { [platform: string]: { url: string; md5: string | null } } } } = {};
         const platforms = ['win32', 'linux', 'darwin'];
         const architectures = ['x86_64', 'arm64'];
 
+        let versionIndex = 1;
+
         for (const version of versionsList) {
-            versionsData[version] = {};
+            const versionLabel = `v${String(versionIndex).padStart(2, '0')}`;
+            versionsData[versionLabel] = {};
+            versionsData[versionLabel][version] = {};
+
             for (const platform of platforms) {
                 for (const arch of architectures) {
                     try {
                         const urlData = gcc.distributionUrl(version, platform, arch);
-                        versionsData[version][`${platform}_${arch}`] = {
+                        versionsData[versionLabel][version][`${platform}_${arch}`] = {
                             url: urlData.url,
                             md5: urlData.md5,
                         };
@@ -24,9 +31,16 @@ async function main() {
                     }
                 }
             }
+            versionIndex++;
         }
 
-        const jsonContent = JSON.stringify(versionsData, null, 2);
+        const reversedVersionsData: { [versionLabel: string]: any } = {};
+        const versionKeys = Object.keys(versionsData).reverse();
+        for (const key of versionKeys) {
+            reversedVersionsData[key] = versionsData[key];
+        }
+
+        const jsonContent = JSON.stringify(reversedVersionsData, null, 2);
         fs.writeFileSync('versions.json', jsonContent, 'utf8');
         console.log('Versions saved to versions.json');
     } catch (error) {
